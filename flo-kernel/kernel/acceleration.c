ad69d9e40655d34d539324bb199af34f566ba305
Jie-Gang Kuang <jk3735@columbia.edu>
2014-10-04 13:01:51 -0400

    Add user-space memory check in the system calls

--- flo-kernel/kernel/acceleration.c
+++ flo-kernel/kernel/acceleration.c
@@ -11,20 +11,25 @@ SYSCALL_DEFINE1(set_acceleration, struct dev_acceleration __user *, acceleration
 	unsigned long sz = sizeof(struct dev_acceleration);
 	static struct dev_acceleration s_kData;
 
-	PRINTK("set_acceleration: old value: %d, %d, %d", 
+	PRINTK("set_acceleration: old value: %d, %d, %d\n",
 		s_kData.x, s_kData.y, s_kData.z);
 
 	if (acceleration == NULL) {
-		PRINTK("set_acceleration NULL pointer param");
+		PRINTK("set_acceleration NULL pointer param\n");
 		return -EINVAL;
 	}
 
-	if (copy_from_user(&s_kData, acceleration, sz) != 0) {
-		PRINTK("set_acceleration memory error");
+	if (!access_ok(VERIFY_READ, acceleration, sz)) {
+		PRINTK("Illigal user-space address\n");
 		return -EFAULT;
 	}
 
-	PRINTK("set_acceleration: new value: %d, %d, %d", 
+	if (copy_from_user(&s_kData, acceleration, sz) != 0) {
+		PRINTK("set_acceleration memory error\n");
+		return -EFAULT;
+	}
+
+	PRINTK("set_acceleration: new value: %d, %d, %d\n",
 		s_kData.x, s_kData.y, s_kData.z);
 
 	return retval;
@@ -33,8 +38,25 @@ SYSCALL_DEFINE1(set_acceleration, struct dev_acceleration __user *, acceleration
 SYSCALL_DEFINE1(accevt_create, struct acc_motion __user *, acceleration)
 {
 	long retval = 0;
+	unsigned long sz = sizeof(struct acc_motion);
+	static struct acc_motion s_kData;
 
-	PRINTK("accevt_create");
+	if (acceleration == NULL) {
+		PRINTK("set_acceleration NULL pointer param\n");
+		return -EINVAL;
+	}
+
+	if (!access_ok(VERIFY_READ, acceleration, sz)) {
+		PRINTK("Illigal user-space address\n");
+		return -EFAULT;
+	}
+
+	if (copy_from_user(&s_kData, acceleration, sz) != 0) {
+		PRINTK("set_acceleration memory error\n");
+		return -EFAULT;
+	}
+
+	PRINTK("accevt_create\n");
 
 	return retval;
 }
@@ -43,7 +65,7 @@ SYSCALL_DEFINE1(accevt_wait, int, event_id)
 {
 	long retval = 0;
 
-	PRINTK("accevt_wait");
+	PRINTK("accevt_wait\n");
 
 	return retval;
 }
@@ -51,8 +73,25 @@ SYSCALL_DEFINE1(accevt_wait, int, event_id)
 SYSCALL_DEFINE1(accevt_signal, struct dev_acceleration __user *, acceleration)
 {
 	long retval = 0;
+	unsigned long sz = sizeof(struct dev_acceleration);
+	static struct dev_acceleration s_kData;
 
-	PRINTK("accevt_signal");
+	if (acceleration == NULL) {
+		PRINTK("set_acceleration NULL pointer param\n");
+		return -EINVAL;
+	}
+
+	if (!access_ok(VERIFY_READ, acceleration, sz)) {
+		PRINTK("Illigal user-space address\n");
+		return -EFAULT;
+	}
+
+	if (copy_from_user(&s_kData, acceleration, sz) != 0) {
+		PRINTK("set_acceleration memory error\n");
+		return -EFAULT;
+	}
+
+	PRINTK("accevt_signal\n");
 
 	return retval;
 }
@@ -61,7 +100,7 @@ SYSCALL_DEFINE1(accevt_destroy, int, event_id)
 {
 	long retval = 0;
 
-	PRINTK("accevt_destroy");
+	PRINTK("accevt_destroy\n");
 
 	return retval;
 }
