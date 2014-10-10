2352777650ad0a05394387058e01cf4f7f0de8a3
Jie-Gang Kuang <jk3735@columbia.edu>
2014-10-10 11:51:58 -0400

    Wait 60 seconds and then destroy all events

--- test/detect_shake.c
+++ test/detect_shake.c
@@ -21,27 +21,11 @@
 #define INIT_FRQ		20
 #define round(x) ((x) >= 0?(long)((x)+0.5):(long)((x)-0.5))
 
-
 struct acc_motion {
 	unsigned int dlt_x;
 	unsigned int dlt_y;
 	unsigned int dlt_z;
 	unsigned int frq;
-};
-
-struct item_setting {
-	unsigned int num;
-	unsigned int min;
-	unsigned int max;
-	unsigned int step;
-};
-
-struct test_setting {
-	unsigned long proc_num;
-	struct item_setting dlt_x;
-	struct item_setting dlt_y;
-	struct item_setting dlt_z;
-	struct item_setting frq;
 };
 
 static void configure_motion(struct acc_motion *ver,
@@ -84,102 +68,97 @@ int main(int argc, char **argv)
 {
 	int retval = 0;
 
-	/* Set up environment variables */
+	int ver_steps = 2;
+	int hor_steps = 2;
+	int shake_steps = 2;
+	float weight = 0.5;
+	pid_t pid;
 
-	dbg("Test: %d starts to dispatch shake detection\n", getpid());
+	int all_steps = hor_steps + ver_steps + shake_steps;
+	struct acc_motion ver;
+	struct acc_motion hor;
+	struct acc_motion shake;
+	unsigned int *evt_id;
+	int i;
 
-	/* Fork child to detect */
-	{
-		int ver_steps = 2;
-		int hor_steps = 2;
-		int shake_steps = 2;
-		float weight = 0.5;
-		pid_t pid;
+	configure_motion(&ver, &hor, &shake);
 
-		int all_steps = hor_steps + ver_steps + shake_steps;
-		struct acc_motion ver;
-		struct acc_motion hor;
-		struct acc_motion shake;
+	evt_id = malloc(sizeof(int) * all_steps);
+	dbg("Test: evt_id =  %X\n", (unsigned int) evt_id);
+	if (evt_id == NULL)
+		return -1;
 
-		configure_motion(&ver, &hor, &shake);
-		unsigned int *evt_id;
-		int i;
-
-		evt_id = malloc(sizeof(int) * all_steps);
-		dbg("Test: evt_id =  %X\n", (unsigned int) evt_id);
-		if (evt_id == NULL)
-			return -1;
-
-		/* create vertical, horizonal, and shake shakeups */
-		dbg("Test: Set up vertical test patern\n");
-		for (i = 0; i < ver_steps; i++) {
-			evt_id[i] = syscall(__NR_accevt_create, &ver);
-			dbg("Test: Created event %d - %d %d %d %d\n",
-			evt_id[i],
-			ver.dlt_x, ver.dlt_y,
-			ver.dlt_z, ver.frq);
-			ver.dlt_x = round(ver.dlt_x * weight);
-			ver.dlt_y = round(ver.dlt_y * weight);
-			ver.dlt_z = round(ver.dlt_z * weight);
-			ver.frq = round(ver.frq * weight);
-		}
-
-		dbg("Test: Set up horizonal test patern\n");
-		for (i = ver_steps; i < hor_steps + ver_steps; i++) {
-			evt_id[i] = syscall(__NR_accevt_create, &hor);
-			dbg("Test: Created event %d - %d %d %d %d\n",
-			evt_id[i],
-			hor.dlt_x, hor.dlt_y,
-			hor.dlt_z, hor.frq);
-			hor.dlt_x = round(hor.dlt_x * weight);
-			hor.dlt_y = round(hor.dlt_y * weight);
-			hor.dlt_z = round(hor.dlt_z * weight);
-			hor.frq = round(hor.frq * weight);
-		}
-
-		dbg("Test: Set up shake test patern\n");
-		for (i = hor_steps + ver_steps; i < all_steps; i++) {
-			evt_id[i] = syscall(__NR_accevt_create, &shake);
-			dbg("Test: Created event %d - %d %d %d %d\n",
-			evt_id[i],
-			shake.dlt_x, shake.dlt_y,
-			shake.dlt_z, shake.frq);
-			shake.dlt_x = round(shake.dlt_x * weight);
-			shake.dlt_y = round(shake.dlt_y * weight);
-			shake.dlt_z = round(shake.dlt_z * weight);
-			shake.frq = round(shake.frq * weight);
-		}
-
-		/* detect events - each event now has 2 processes */
-		dbg("Test: Fork children to wait on events\n");
-		for (i = 0; i < all_steps; i++) {
-			pid = fork();
-			if (pid == 0) {
-				dbg("Test: %d Wait on event %d\n",
-						getpid(), evt_id[i]);
-				retval = syscall(__NR_accevt_wait, evt_id[i]);
-				retval = syscall(__NR_accevt_wait, evt_id[i]);
-
-				if (i < ver_steps)
-					print_vshake();
-				else if (i < hor_steps + ver_steps)
-					print_hshake();
-				else if (i < all_steps)
-					print_shake();
-				dbg("Test: Destroy event %d\n", evt_id[i]);
-				retval = syscall(__NR_accevt_destroy,
-						evt_id[i]);
-				return retval;
-			} else if (pid < 0) {
-				retval = pid;
-			}
-		}
-
-		while (wait(NULL) >= 0)
-			dbg("Test: %d all children are waited\n", getpid());
-
+	/* create vertical, horizonal, and shake shakeups */
+	dbg("Test: Set up vertical test patern\n");
+	for (i = 0; i < ver_steps; i++) {
+		evt_id[i] = syscall(__NR_accevt_create, &ver);
+		dbg("Test: Created event %d - %d %d %d %d\n",
+		evt_id[i],
+		ver.dlt_x, ver.dlt_y,
+		ver.dlt_z, ver.frq);
+		ver.dlt_x = round(ver.dlt_x * weight);
+		ver.dlt_y = round(ver.dlt_y * weight);
+		ver.dlt_z = round(ver.dlt_z * weight);
+		ver.frq = round(ver.frq * weight);
 	}
 
-	return retval;
-}
+	dbg("Test: Set up horizonal test patern\n");
+	for (i = ver_steps; i < hor_steps + ver_steps; i++) {
+		evt_id[i] = syscall(__NR_accevt_create, &hor);
+		dbg("Test: Created event %d - %d %d %d %d\n",
+		evt_id[i],
+		hor.dlt_x, hor.dlt_y,
+		hor.dlt_z, hor.frq);
+		hor.dlt_x = round(hor.dlt_x * weight);
+		hor.dlt_y = round(hor.dlt_y * weight);
+		hor.dlt_z = round(hor.dlt_z * weight);
+		hor.frq = round(hor.frq * weight);
+	}
 
+	dbg("Test: Set up shake test patern\n");
+	for (i = hor_steps + ver_steps; i < all_steps; i++) {
+		evt_id[i] = syscall(__NR_accevt_create, &shake);
+		dbg("Test: Created event %d - %d %d %d %d\n",
+		evt_id[i],
+		shake.dlt_x, shake.dlt_y,
+		shake.dlt_z, shake.frq);
+		shake.dlt_x = round(shake.dlt_x * weight);
+		shake.dlt_y = round(shake.dlt_y * weight);
+		shake.dlt_z = round(shake.dlt_z * weight);
+		shake.frq = round(shake.frq * weight);
+	}
+
+	/* detect events - each event now has 2 processes */
+	dbg("Test: Fork children to wait on events\n");
+	for (i = 0; i < all_steps; i++) {
+		pid = fork();
+		if (pid == 0) {
+			dbg("Test: %d Wait on event %d\n",
+					getpid(), evt_id[i]);
+			retval = syscall(__NR_accevt_wait, evt_id[i]);
+
+			if (i < ver_steps)
+				print_vshake();
+			else if (i < hor_steps + ver_steps)
+				print_hshake();
+			else
+				print_shake();
+			dbg("Test: Destroy event %d\n", evt_id[i]);
+			retval = syscall(__NR_accevt_destroy,
+					evt_id[i]);
+			return retval;
+		} else if (pid < 0) {
+			retval = pid;
+		}
+	}
+
+	sleep(60);
+	/* Destroy all event */
+	for (i = 0; i < all_steps; i++)
+		retval syscall(__NR_accevt_destroy, evt_id[i]);
+
+	while (wait(NULL) >= 0)
+		dbg("Test: %d all children are waited\n", getpid());
+
+	return 0;
+}
